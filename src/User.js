ccacf8e9df707c0f1dcc2f70f90398508bdffbe8
Huy Pham <cocwedc@gmail.com>
2018-11-17 16:45:22 -0600

    backup

--- src/User.js
+++ src/User.js
@@ -29,7 +29,7 @@ class User extends Component {
             },
             {
               "url": "https://gogts.net/wp-content/uploads/2018/05/2018-Topps-Finest-Baseball-Cards-3.jpg",
-              "title": "Plato is not a philosopher, he is a plain thinker"
+              "title": "Plato is not a philosopher, he is a plain thinker."
             },
           ]
     }
@@ -41,19 +41,19 @@ class User extends Component {
 
     return (
       <div className="container-fluid">
-        <div className="App ">
+        <div className="App">
           <Navbar />
           <div className="container m-profile-user-frame">
             <div className="">
               <img className="rounded m-profile-avatar float-left" src="https://i.pinimg.com/736x/e0/06/f3/e006f3de43c580458aebf3e7f20d526f--tedy-bruschi-arizona-wildcats.jpg" alt="Card image cap" />
-              <t className="m-profile-name">Rob Gronkowski</t>
+              <t className="m-profile-name">Aleksandre Gronkowski</t>
               <span className="m-profile-username">@gronk88</span>
             </div>
           </div>
 
           <div className="content-main">
 
-            <div className="container">
+            <div className="container" style={{backgroundColor: 'green'}}>
               <button class="btn navbar-button" type="submit">Decks</button>
               <button class="btn navbar-button" type="submit">Activity</button>
               <button class="btn navbar-button" type="submit">Following</button>
