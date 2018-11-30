1dacc52b2189ca218ed1dee1cdfa8b863c5e5408
Huy Pham <cocwedc@gmail.com>
2018-11-30 10:15:33 -0600

    upload to test

--- src/components/AuthenLogin.js
+++ src/components/AuthenLogin.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import '../styles/Home.css';
 import '../styles/User.css';
 import '../styles/AuthenLogin.css'
 
 class AuthenLogin extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <div className="m-lm-background-login">
--- src/components/AuthenRegister.js
+++ src/components/AuthenRegister.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import '../styles/Home.css';
 import '../styles/User.css';
 import '../styles/AuthenLogin.css'
@@ -6,6 +7,39 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import AuthenLogin from './AuthenLogin.js';
 
 class AuthenRegister extends Component {
+
+    registerNewUser() {
+        console.log('testicle')
+        axios.post(`http://127.0.0.1:5000/register`, {
+            "​name": "Mason Dixon",
+            "​username": "mason1",
+            "​password": "dixon",
+            "​confirm_password": "dixon"
+        })
+        .then(function(response) {
+          console.log(response);
+          console.log("Thanh Cong")
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      }
+
+    testfunx() {
+        axios.get(`http://127.0.0.1:5000/testuser`, { 
+            headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+            },
+            }).then(response => {
+                // If request is good...
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log('error 3 ' + error);
+            });
+    }
+
     render() {
         return (
           <div className="m-lm-background-signup">
@@ -15,11 +49,14 @@ class AuthenRegister extends Component {
                 <hr/>
                 <input className="form-control mr-sm-2 m-lm-input" type="search" placeholder="Username" aria-label="username" />
                 <input className="form-control mr-sm-2 m-lm-input" type="search" placeholder="Password" aria-label="password" />
-                <a href="https://google.com">
-                    <button className="btn form-control mr-sm-2 m-lm-button" style={{backgroundColor: 'rgb(255,45,85)', color: '#eceff1'}}>
-                    Sign up
+
+                {/* Button */}
+                <form onSubmit={this.testfunx}>
+                    <button type="submit" className="btn form-control mr-sm-2 m-lm-button" style={{backgroundColor: 'rgb(255,45,85)', color: '#eceff1'}}>
+                        Sign up
                     </button>
-                </a>
+                </form>
+
                 <hr/>
                 <t className="m-lm-text">
                     Already have an account?
