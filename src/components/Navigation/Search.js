1fec8e84ab6439d65b1efaaf84ac232e257e6cdf
Huy Pham <cocwedc@gmail.com>
2018-12-28 18:38:51 +0700

    temp

--- src/components/Navigation/Search.js
+++ src/components/Navigation/Search.js
@@ -5,9 +5,7 @@ import '../../styles/AuthenLogin.css'
 import '../../styles/Home.css'
 import '../../styles/User.css'
 
-const updateByPropertyName = (propertyName, value) => () => ({
-    [propertyName]: value,
-});
+import { goToCard } from '../Authentication/AuthenStatus.js'
 
 class Search extends Component {
     constructor(props) {
@@ -21,29 +19,60 @@ class Search extends Component {
 
     querySearch(term) {
         console.log(term)
+
+        const data = {
+            'term': term
+        }
+
+        axios.post('http://127.0.0.1:5000/search', data)
+        .then(res => {
+            console.log(res)
+            this.setState({ query: res.data })
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
 
     render() {
 
-        const { term } = this.state
+        const { term, query } = this.state
+        const cardUrl = 'http://127.0.0.1:5000/static/CardPicture/';
+
+        const updateByPropertyName = (propertyName, value) => () => ({
+            [propertyName]: value,
+        });
 
         return(
-            <div className="container">
-                <div className="row col-12 justify-content-center">
-                    <div className="col-lg-8 col-md-10 col-md-*">
+
+            <div className="container-fluid">
+
+                <div className="row text-align-center justify-content-center">
+                    <div className="m-profile-container col-lg-6 col-md-10 col-sm-* col-xs-*">
                         <input
                             className="m-lm-ghost-search"
                             value={term}
                             onChange={event => {
                                 this.setState(updateByPropertyName('term', event.target.value))
-                                this.querySearch(term)
                             }}
                             type="text"
                             placeholder="Add A Title"
                         />
-                        {/* <button className="mr-sm-2 m-lm-create-card-button rounded" disabled={isInvalid} type="submit">
-                            Submit Post
-                        </button> */}
+                    </div>
+                </div>
+
+
+                <div className="row text-align-center justify-content-center">
+                    <div className="m-profile-card-container col-lg-6 col-md-10 col-sm-* col-xs-*">
+                        {query.slice(0).reverse().map(function (post, i) { // reverse array map function with server data
+                            return (
+                            <div className="m-profile-whole-card-cover rounded" key={i}>
+                                <img onClick={() => goToCard(post.id)} className="card-img-top m-profile-card-cover rounded" src={cardUrl + post.picture} alt="" />
+                                <p onClick={() => goToCard(post.id)} className="m-user-card-text">{post.title}</p>
+                                {/* <p className="m-user-desc-text">{post.description}</p> */}
+                            </div>
+                            )
+                        })}
                     </div>
                 </div>
             </div>
