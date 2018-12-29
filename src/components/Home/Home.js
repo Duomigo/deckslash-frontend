48891111e5e2b329f88d32b921a7f45bccb5a90d
Huy Pham <cocwedc@gmail.com>
2018-12-29 21:24:08 +0700

    halle

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -41,33 +41,29 @@ class Home extends Component {
 
     return (
 
-      <div>
-
-        <div className="container-fluid">
+      <div className="container-fluid">              
+        <div className="row text-align-center justify-content-center">
           <div className="row col-12 justify-content-center">
-            <div className="col-lg-8 col-md-10 col-sm-* col-xs-*">
+            <div className="col-lg-6 col-md-8 col-sm-10 col-xs-*">
               <h1 className="m-home-latest">Latest reviews</h1>
             </div>
           </div>
 
-          <div className="row text-align-center justify-content-center">
-            <div className="m-home-card-container col-lg-8 col-md-10 col-sm-* col-xs-*">
-              {posts.slice(0).reverse().map(function (post, i) { // reverse array map function with server data
-                return (
-                  <div className="m-profile-whole-card-cover rounded" key={i}>
-                    <img onClick={() => goToCard(post.id)} className="card-img-top m-profile-card-cover rounded" src={cardUrl + post.picture} alt="" />
-                    <p onClick={() => goToCard(post.id)} className="m-user-card-text">{post.title}</p>
-                    {/* <p onClick={() => goToCard(post.id)} className="m-user-card-text">{this.getCurrentUser(post.author)}</p> */}
-                    {/* <p className="m-user-desc-text">{post.description}</p> */}
-                  </div>
-                )
-              })}
-            </div>
+          <div className="m-profile-card-container col-lg-6 col-md-8 col-sm-10 col-xs-*">           
+            {posts.slice(0).reverse().map(function (post, i) { // reverse array map function with server data
+              return (
+                <div className="m-profile-whole-card-cover rounded" key={i}>
+                  <img onClick={() => goToCard(post.id)} className="card-img-top m-profile-card-cover rounded" src={cardUrl + post.picture} alt="" />
+                  <p onClick={() => goToCard(post.id)} className="m-user-card-text">{post.title}</p>
+                  {/* <p onClick={() => goToCard(post.id)} className="m-user-card-text">{this.getCurrentUser(post.author)}</p> */}
+                  {/* <p className="m-user-desc-text">{post.description}</p> */}
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>
         
-
     );
   }
 }
