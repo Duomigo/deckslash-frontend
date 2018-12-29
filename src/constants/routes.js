859d791dc68cb3e9db7cea5ef0fffcd946c64162
Huy Pham <cocwedc@gmail.com>
2018-12-29 09:49:05 +0700

    rspw

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,6 +2,7 @@ export const LANDING = '/';
 export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
 export const PASSWORD_FORGET = '/pw-forget';
+export const RESET_PASSWORD = '/reset-pw'
 export const HOME = '/home';
 export const NEW = '/new'
 export const SEARCH = '/search'
