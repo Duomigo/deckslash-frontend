4f5e938fcf7aad844d8dbee2bf1611724ec1cb93
Huy Pham <cocwedc@gmail.com>
2018-12-29 15:52:56 +0700

    halle

--- src/components/Navigation/GuestNavbar.js
+++ src/components/Navigation/GuestNavbar.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from '../../images/dplogo.svg';
+import logo from '../../images/mojito.svg';
 import '../../styles/Home.css';
+
+import profileB from '../../images/m-user.svg'
+import searchB from '../../images/m-search.svg'
 
 import * as routes from '../../constants/routes';
 import {
@@ -16,19 +19,21 @@ class GuestNavbar extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-style">
         <a className="navbar-brand" href="/">
-          <img src={logo} width="125" height="25" alt="Deckslash-logo" />
+          <img src={logo} width="150" height="30" alt="Deckslash-logo" />
         </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-
-          </ul>
-            <Link className="navbar-button" to={routes.SEARCH}>Search</Link>
-            <Link className="navbar-button" to={routes.SIGN_IN}>Log In</Link>
-            <Link className="navbar-button" to={routes.SIGN_UP}>Sign Up</Link>
+        <div className="navbar-nav ml-auto" id="navbarSupportedContent">
+          <div className="row">
+            <Link className="navbar-button" to={routes.SEARCH}>
+              <img src={searchB} width="25" height="25" alt="Profile"/>
+            </Link>
+            <Link className="navbar-button" to={routes.SIGN_IN}>
+              <img src={profileB} width="25" height="25" alt="Profile"/>
+            </Link>
+          </div>
         </div>
       </nav>
     )
--- src/components/Navigation/UserNavbar.js
+++ src/components/Navigation/UserNavbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from '../../images/dplogo.svg';
+import logo from '../../images/mojito.svg';
 import '../../styles/Home.css';
 import axios from 'axios';
 
@@ -9,6 +9,7 @@ import SignOutButton from '../Authentication/SignOut';
 import * as routes from '../../constants/routes';
 import newB from '../../images/m-new.svg'
 import profileB from '../../images/m-user.svg'
+import searchB from '../../images/m-search.svg'
 
 import {
     BrowserRouter as Router,
@@ -46,23 +47,25 @@ class UserNavbar extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-style">
         <a className="navbar-brand" href="/">
-          <img src={logo} width="125" height="25" alt="Deckslash-logo" />
+          <img src={logo} width="150" height="30" alt="Deckslash-logo" />
         </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-
-          </ul>
+        <div className="navbar-nav ml-auto" id="navbarSupportedContent">
+          <div className="row">
+            <Link className="navbar-button" to={routes.SEARCH}>
+              <img src={searchB} width="25" height="25" alt="New"/>
+            </Link>
             <Link className="navbar-button" to={routes.NEW}>
               <img src={newB} width="25" height="25" alt="New"/>
             </Link>
             <a className="navbar-button" href={profilePath}>
               <img src={profileB} width="25" height="25" alt="Profile"/>
             </a>
-            <SignOutButton />
+            <SignOutButton />          
+          </div>
         </div>
       </nav>
     )
